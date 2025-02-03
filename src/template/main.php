3e897dcdc8ec824721d8c6dfb0729699ae9bf084
Matheus A. Barbosa <mafeus.barbosa@gmail.com>
2025-02-03 14:58:58 -0300

    move chart scripts

--- src/template/main.php
+++ src/template/main.php
@@ -75,65 +75,7 @@ function htmlAppliedRector($fqn)
                     })
                 }
 
-                document.addEventListener('DOMContentLoaded', function() {
 
-                    enableCodeDiffOn(document.querySelector('.tab-pane.active'))
-
-                    const chartEl = document.getElementById('chart');
-                    const labels = <?=json_encode($graphLabels);?>;
-                    const values = <?=json_encode($graphDiffOccurrences);?>;
-                    const colors = [
-                        '#118fff',
-                        '#36b9ff',
-                        '#3800ff',
-                        '#5319ff',
-                        '#4b7aff',
-                        '#d536c0',
-                        '#dd54ff',
-                        '#e5afff',
-                        '#16ffff',
-                        '#0b7562',
-                        '#005149',
-                        '#b55c39',
-                        '#ba775e',
-                        '#d3a698',
-                    ];
-                    const data = {
-                        labels: labels,
-                        datasets: [
-                            {
-                                label: 'Ocurrences',
-                                data: values,
-                                borderWidth: 1,
-                                borderColor: 'transparent',
-                                backgroundColor: colors
-                            }
-                        ]
-                    }
-                    const config = {
-                        type: 'bar',
-                        data: data,
-                        options: {
-                            scales: {
-                                x: {
-                                    ticks: {
-                                        display: false,
-                                        z: 9999,
-                                        padding: -100
-                                    }
-                                },
-                            },
-                            responsive: false,
-                            plugins: {
-                                legend: {
-                                    display: false,
-                                },
-                            }
-                        },
-                    };
-
-                    new Chart(chartEl, config);
-                });
             </script>
             <div class="row" id="stats-row">
                 <?php
@@ -157,6 +99,5 @@ function htmlAppliedRector($fqn)
             integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
             crossorigin="anonymous"
         ></script>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     </body>
 </html>
