678deac28195e3cea35fd10915c3631c060ead5c
Matheus A. Barbosa <mafeus.barbosa@gmail.com>
2025-02-03 21:00:17 -0300

    fix diff card overflow

--- src/template/changes_by_file/card.php
+++ src/template/changes_by_file/card.php
@@ -7,27 +7,28 @@ $diff = htmlentities($file['diff']);
     <div class="card-header">
         <h5><code><?= $file['file'] ?></code></h5>
     </div>
-    <div class="row p-0">
-        <div class="col-xxl-3 pe-0">
-            <div class="p-2 pe-0">
-                <h6 class="mt-xl-0 mt-lg-3 text-uppercase">Applied Rectors</h6>
-                <ul class="list-group list-group-flush">
-                    <?php
-                    foreach ($file['applied_rectors'] as $fqn): ?>
-                        <li class="list-group-item"><?= htmlAppliedRector($fqn) ?></li>
-                    <?php
-                    endforeach ?>
-                </ul>
+    <div class="card-body p-0 overflow-hidden">
+        <div class="row">
+            <div class="col-xxl-3 pe-0">
+                <div class="p-3 pe-0">
+                    <h6 class="mt-2 text-uppercase">Applied Rectors</h6>
+                    <ul class="list-group list-group-flush">
+                        <?php
+                        foreach ($file['applied_rectors'] as $fqn): ?>
+                            <li class="list-group-item"><?= htmlAppliedRector($fqn) ?></li>
+                        <?php
+                        endforeach ?>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-xxl-9">
+                <div class="code-diff-container" data-content="<?= $diff ?>">
+                    <code>
+                        <pre><?= $diff ?></pre>
+                    </code>
+                </div>
             </div>
         </div>
-
-        <div class="col-xxl-9">
-            <div class="code-diff-container" data-content="<?= $diff ?>">
-                <code>
-                    <pre><?= $diff ?></pre>
-                </code>
-            </div>
-        </div>
-
     </div>
 </div>
